8e0e15d76f158feedd80cf14d5a2ad01151e08da
leonguevara <leon.guevara@outlook.com>
2016-08-31 16:30:31 -0500

    Initial Commit

---
+++ PruebaTallerGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaTallerGit
+//
+//  Created by León Felipe Guevara Chávez on 8/31/16.
+//  Copyright © 2016 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
